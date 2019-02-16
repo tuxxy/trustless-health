87db0890aa4558b089e425995a8cbf261cfbba44
jetarex <43651330+jetarex@users.noreply.github.com>
2019-02-15 21:16:49 -0700

    add encrypt function

--- client-server/__main__.py
+++ client-server/__main__.py
@@ -2,7 +2,7 @@
 #
 # For license information, please see the LICENSE file in the root directory.
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 import nufhe
@@ -12,12 +12,20 @@ ctx = nufhe.Context()
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/generate_secret_key")
+@app.route("/generate_secret_key",methods=['POST'])
 def generate_secret_key():
     secret_key = ctx.make_secret_key()
     obj = {}
     obj["secret_key"] = binascii.hexlify(secret_key.dumps()).decode('ascii')
-    return json.dumps(obj)
+    return jsonify(obj)
+
+@app.route('/encrypt',methods=['POST'])
+def encrypt():
+    data = request.json
+    secret_key = nufhe.NuFHESecretKey.loads(binascii.unhexlify(data['secret_key']), ctx.thread)
+    data_to_encrypt = data['data']
+    ciphertext = ctx.encrypt(secret_key, data_to_encrypt)
+    return jsonify({"result":"success", "data": {"ciphertext": binascii.hexlify(ciphertext.dumps()).decode('ascii')}})
 
 if __name__ == "__main__":
     app.run(debug=True)
