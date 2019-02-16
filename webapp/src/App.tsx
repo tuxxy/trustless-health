e34e3c5a719043eb25692c7e91f04a799450b8f3
hallex-br <mads@hallex.com>
2019-02-15 20:29:31 -0700

    Blueprint added

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react';
 import './App.css';
 
+import {Button, Intent } from "@blueprintjs/core";
 import logo from './logo.svg';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Trustless Health</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to Trustless Health</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                    <Button onClick={this.handleClick} intent={Intent.PRIMARY}>Click</Button>
+                </p>
+            </div>
+        );
+    }
+
+    private handleClick() {
+        alert('Hello world!');
+    }
 }
 
 export default App;
