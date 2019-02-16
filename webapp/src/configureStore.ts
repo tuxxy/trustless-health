b1f1450e6629829dbb6bbb24c4227e88c471bda5
hallex-br <mads@hallex.com>
2019-02-15 22:14:12 -0700

    Redux with dev tools now working

--- webapp/src/configureStore.ts
+++ webapp/src/configureStore.ts
@@ -1,11 +1,13 @@
 import {applyMiddleware, createStore, Store} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger'
 import reduxThunk from 'redux-thunk';
 import {IRootState, rootReducer} from './reducers';
 
 const configureStore = (initialState?: IRootState): Store<IRootState | undefined> => {
-    const middlewares = [reduxThunk];
-
-    return createStore(rootReducer, initialState, applyMiddleware(...middlewares));
+    const middlewares = [reduxThunk, logger];
+    const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
+    return createStore(rootReducer, initialState, enhancer);
 };
 
 const store = configureStore();
--- webapp/src/tests/App.test.tsx
+++ webapp/src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Main from '../components/Main';
+import Main from '../containers/mainContainer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
