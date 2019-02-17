7c1d810895a7cf15c06c809b673964cb51e767e4
hallex-br <mads@hallex.com>
2019-02-17 02:17:29 -0700

    Progress pop up for compute

--- webapp/src/components/computeProgress.tsx
+++ webapp/src/components/computeProgress.tsx
@@ -20,31 +20,52 @@ export class ComputeProgress extends React.Component<IProps> {
                 icon="database"
                 onClose={this.handleClose}
                 title="Computation Status"
-                style={{height: '50vh', width: '30vh'}}
+                style={{minHeight: '500px', minWidth: '700px'}}
                 isOpen={computeProgressOpen}
+                canEscapeKeyClose={false}
+                canOutsideClickClose={false}
             >
                 <div className={Classes.DIALOG_BODY}>
-                    {decryptedComputedData !== '' && <Spinner/>}
-                    <h4>The server is working hard on your data.</h4>
 
-                    <Callout icon="info-sign" intent="none" title="The data to analyse">
-                        {dataToEncrypt.slice(0, 10)}...
+                    <Callout icon="info-sign" intent="none" title="Data to analyse">
+                        <div className="flex-grid">
+                            <div className="col">
+                                {dataToEncrypt.slice(0, 20)}...
+                            </div>
+                            <div className="col callout-spinner">
+                                {encryptedData === '' && <Spinner/>}
+                            </div>
+                        </div>
                     </Callout>
-
+                    <br/>
                     {encryptedData !== '' && <>
-                    <Callout icon="info-sign" intent="none" title="The data encrypted">
-                        {dataToEncrypt.slice(0, 10)}...
+                    <Callout icon="info-sign" intent="none" title="Encrypted data">
+                        <div className="flex-grid">
+                            <div className="col">
+                                {encryptedData.slice(0, 20)}...
+                            </div>
+                            <div className="col callout-spinner">
+                                {encryptedComputedData === '' && <Spinner/>}
+                            </div>
+                        </div>
                     </Callout>
                     </>}
-
+                    <br/>
                     {encryptedComputedData !== '' && <>
-                        <Callout icon="info-sign" intent="none" title="The computed encrypted data">
-                            {encryptedComputedData.slice(0, 10)}...
+                        <Callout icon="info-sign" intent="none" title="Computed encrypted data">
+                            <div className="flex-grid">
+                                <div className="col">
+                                    {encryptedComputedData.slice(0, 20)}...
+                                </div>
+                                <div className="col callout-spinner">
+                                    {decryptedComputedData === '' && <Spinner/>}
+                                </div>
+                            </div>
                         </Callout>
                     </>}
-
+                    <br/>
                     {decryptedComputedData !== '' && <>
-                        <Callout icon="info-sign" intent="success" title="The decrypted result">
+                        <Callout icon="info-sign" intent="success" title="Decrypted result">
                             {decryptedComputedData}
                         </Callout>
                     </>}
@@ -53,13 +74,13 @@ export class ComputeProgress extends React.Component<IProps> {
                     <div className={Classes.DIALOG_FOOTER_ACTIONS}>
                         {decryptedComputedData === '' ?
                             <>
-                                <Tooltip content="Close this window" intent={Intent.WARNING}>
-                                    <Button onClick={this.handleClose}>Quit</Button>
+                                <Tooltip content="Close this window">
+                                    <Button onClick={this.handleClose} intent={Intent.DANGER}>Quit</Button>
                                 </Tooltip>
                             </> :
                             <>
-                                <Tooltip content="Close this window" intent={Intent.PRIMARY}>
-                                    <Button onClick={this.handleClose}>Close</Button>
+                                <Tooltip content="Close this window">
+                                    <Button onClick={this.handleClose} intent={Intent.PRIMARY}>Close</Button>
                                 </Tooltip>
                             </>
                         }
